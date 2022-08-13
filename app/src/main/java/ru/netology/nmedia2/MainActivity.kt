09e413c17b120a86c12fd0c0e44343b54ba45686
uri752 <uri752blag@mail.ru>
2022-08-13 17:01:40 +0300

    task3 add Layout

--- app/src/main/java/ru/netology/nmedia2/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia2/MainActivity.kt
@@ -3,6 +3,8 @@ package ru.netology.nmedia2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// path to sample data : https://github.com/netology-code/and2-code/tree/master/03_constraintlayout/app/sampledata
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
